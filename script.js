9ce83cf98134f808f76699d3fe846b8c59acd0bf
yashhu666 <yash40gsssktl@gmail.com>
2024-05-11 15:35:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+window.addEventListener('scroll', function() {
+  var elements = document.querySelectorAll('#h1, #h2, #who, #art, #cntct, #background, #discord');
+  var windowHeight = window.innerHeight;
+
+  elements.forEach(function(element) {
+    var elementTop = element.getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      element.classList.add('visible');
+    } else {
+      element.classList.remove('visible');
+    }
+  });
+});
